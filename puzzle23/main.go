b5e8f4cbff75451267849505a9bdea33308d1919
Dan Liberatori <daniel.liberatori@gmail.com>
2019-12-29 00:24:56 +0900

    Solved puzzle 23 pt 2

--- puzzle23/main.go
+++ puzzle23/main.go
@@ -14,13 +14,19 @@ type packet struct {
 }
 
 type network struct {
-	nodes     []intcode.Computer
-	nodeCount int
+	nodeCount        int
+	natBuffer        packet // stores a packet that the NAT may route as needed in order to manage traffic
+	natBufferHistory int    // remember the last y value sent by the NAT
+	nodes            []intcode.Computer
 }
 
 func makeNetwork(nodeCount int, NIC string) network {
 
-	net := network{nodes: make([]intcode.Computer, nodeCount), nodeCount: nodeCount}
+	net := network{
+		nodes:            make([]intcode.Computer, nodeCount),
+		nodeCount:        nodeCount,
+		natBufferHistory: -9999,
+	}
 
 	for i := 0; i < nodeCount; i++ {
 		net.nodes[i] = intcode.MakeComputer(NIC, nil, nil)
@@ -36,9 +42,16 @@ func (net *network) tick() {
 	}
 }
 
-func (net *network) routePackets() {
-	msgQueues := make(map[int][]int)
+func (net *network) routePackets() bool {
+	// an easy way of letting the caller know whether the
+	// puzzle condition has been met.
+	exitConditionFlag := false
 
+	// we need to keep track of what nodes received messages
+	// and modify some of them in-flight depending on the
+	// network circumstances,
+	// so we accumulate all messages before distributing them
+	msgQueues := make(map[int][]int)
 	for i := 0; i < net.nodeCount; i++ {
 		msgQueues[i] = make([]int, 0)
 	}
@@ -48,8 +61,16 @@ func (net *network) routePackets() {
 		packets := unpackPacketBuffer(buffer)
 
 		for _, p := range packets {
-			if p.destination < 0 || p.destination >= net.nodeCount {
+			if (p.destination < 0 || p.destination >= net.nodeCount) && p.destination != 255 {
+				// this should not happen
 				fmt.Println("Invalid destination address.", p)
+				continue
+			} else if p.destination == 255 {
+				// Packages routed to the NAT.
+				// Only remember the last one received.
+				net.natBuffer.destination = p.destination
+				net.natBuffer.x = p.x
+				net.natBuffer.y = p.y
 				continue
 			}
 			msgQueues[p.destination] = append(msgQueues[p.destination], p.x)
@@ -58,14 +79,32 @@ func (net *network) routePackets() {
 
 	}
 
-	for i := 0; i < net.nodeCount; i++ {
+	emptyCount := 0
+	for i := net.nodeCount - 1; i >= 0; i-- {
+
 		// any nodes that did not receive input during this cycle get a -1
-		if len(msgQueues[i]) == 0 {
+		// EXCEPT for the case where ALL nodes are idle.
+		// If all nodes are idle (i.e., if all nodes will receive a -1),
+		// then node 0 receives the package that is currently stored in the NAT
+		if emptyCount < net.nodeCount-1 && len(msgQueues[i]) == 0 {
 			msgQueues[i] = append(msgQueues[i], -1)
+			emptyCount++
+
+		} else if emptyCount == net.nodeCount-1 && len(msgQueues[i]) == 0 {
+
+			msgQueues[i] = append(msgQueues[i], net.natBuffer.x)
+			msgQueues[i] = append(msgQueues[i], net.natBuffer.y)
+			emptyCount++
+
+			// fmt.Println(net.natBuffer)
+			exitConditionFlag = net.natBufferHistory == net.natBuffer.y
+			net.natBufferHistory = net.natBuffer.y
 		}
 
 		net.nodes[i].QueueInput(msgQueues[i]...)
 	}
+
+	return exitConditionFlag
 }
 
 func unpackPacketBuffer(buffer []int) []packet {
@@ -92,12 +131,36 @@ func part1() {
 
 	net := makeNetwork(50, string(data))
 
-	for i := 0; i < 10000; i++ {
+	for true {
 		net.tick()
 		net.routePackets()
+		if net.natBuffer.y != 0 {
+			break
+		}
 	}
+
+	fmt.Println("[Part 1] The y value of the first packet sent to the NAT is", net.natBuffer.y)
+}
+
+func part2() {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic("Can't read input file.")
+	}
+
+	net := makeNetwork(50, string(data))
+
+	for true {
+		net.tick()
+		if net.routePackets() {
+			break
+		}
+	}
+
+	fmt.Println("[Part 2] The first repeated y value sent by the NAT is", net.natBuffer.y)
 }
 
 func main() {
 	part1()
+	part2()
 }
